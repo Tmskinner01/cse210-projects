49215f20f9201e94fa4b1c6b42387529a8d4a303
Tmskinner01 <tmskinner01@gmail.com>
2023-06-12 17:30:24 -0400

    Finished learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // create shapes
+        Square s1 = new Square("Blue", 7);
+        Rectangle r1 = new Rectangle("Green", 5, 8);
+        Circle c1 = new Circle("Red", 3);
+        
+        // create shapes list and add shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s1);
+        shapes.Add(r1);
+        shapes.Add(c1);
+
+        //iterate through list
+        foreach(Shape s in shapes)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
+
+
     }
 }
